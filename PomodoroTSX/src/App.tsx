ead03dc5adc2b9ce950e5307c67e7548877f7fa8
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-01-11 16:25:04 -0300

    Router Provider added

--- PomodoroTSX/src/App.tsx
+++ PomodoroTSX/src/App.tsx
@@ -2,12 +2,15 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { ThemeProvider } from 'styled-components'
 import { Router } from './Components/Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
-      <Router/>
     </ThemeProvider>
   )
 }
